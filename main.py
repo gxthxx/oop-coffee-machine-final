979b5556e7cec1360a85f7476b5a144910cb818d
Andrew Díaz <95590535+gxthxx@users.noreply.github.com>
2023-11-13 12:35:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         
-        k)if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-          coffee_maker.make_coffee(drin)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
